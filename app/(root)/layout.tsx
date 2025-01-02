367067dc2aaefeb4ce76c5a52cb7a4c1a40784d1
Brady Mostoller <bradymostoller26@gmail.com>
2025-01-02 11:42:26 -0700

    Add files via upload

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,37 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const loggedIn = await getLoggedInUser();
+
+  //if(!loggedIn)  redirect('/sign-in')
+
+
+
+  return (
+    <main className = "flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg"
+            width={30} height={30} alt="logo"/>
+            <div>
+              <MobileNav 
+              user={loggedIn}
+              />
+            </div>
+          </div>
+        </div>
+        {children}
+    </main>
+  );
+}
